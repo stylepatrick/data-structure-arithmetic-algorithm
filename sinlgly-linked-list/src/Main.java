81843af3fc3f264e101b32fd3f0844b85086047d
stylepatrick <ahrntal.patrick@gmail.com>
2022-03-18 22:26:10 +0100

    add singly linked list

---
+++ sinlgly-linked-list/src/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+
+    public static void main(String args[]) {
+        MyList myList = new MyList();
+        // Append values to last position from list
+        myList.append(1);
+        myList.append(2);
+        myList.append(3);
+
+        // Insert values at the beginning of the list
+        myList.insert(0);
+        myList.insert(-1);
+
+        myList.append(4);
+
+        System.out.println("-------- Lookup in List ----------");
+        System.out.println(myList.exist(2));
+        System.out.println(myList.exist(22));
+
+        System.out.println("-------- Print full List ----------");
+        myList.printList();
+
+        // Find and delete value from list
+        myList.delete(3);
+
+        // Insert value at specific position
+        myList.insertAt(-9, 2);
+
+        // Delete at specific position
+        myList.deleteAt(2);
+
+        // Delete last value
+        myList.deleteLast();
+
+        // Delete first value
+        myList.deleteFirst();
+
+        myList.delete(0);
+
+        System.out.println("-------- Print full List ----------");
+        myList.printList();
+
+    }
+}
---
+++ sinlgly-linked-list/src/MyList.java
@@ -0,0 +1,146 @@
+public class MyList {
+
+    Node tail;
+    Node head;
+
+    void insert(int val) {
+        Node h = head;
+        Node p = new Node(val);
+        p.next = h;
+        head = p;
+    }
+
+    void append(int val) {
+        Node p = new Node(val);
+        if (head == null) {
+            head = p;
+        } else {
+            tail.next = p;
+        }
+        tail = p;
+    }
+
+    void delete(int val) {
+        Node h = head;
+        Node prev = null;
+        if (h != null) {
+            while (h != null && h.val != val) {
+                prev = h;
+                h = h.next;
+            }
+            if (h != null) {
+                if (h == head) {
+                    head = h.next;
+                } else {
+                    prev.next = h.next;
+                }
+            }
+            
+        } else {
+            System.out.println("List empty!");
+        }
+
+    }
+
+    void insertAt(int val, int position) {
+        Node h = head;
+        Node next;
+        Node p = new Node(val);
+        if (position == 0) {
+            next = head;
+            head = p;
+            p.next = next;
+
+        } else {
+            int i = 1;
+            if (h != null) {
+                while (h.next != null) {
+                    if (i == position) {
+                        next = h.next;
+                        h.next = p;
+                        p.next = next;
+                        return;
+                    }
+                    i++;
+                    h = h.next;
+                }
+                System.out.println("Position out of range!");
+            }
+        }
+    }
+
+    void deleteAt(int position) {
+        Node h = head;
+        Node prev = null;
+        if (position == 0) {
+            head = head.next;
+        } else {
+            int i = 0;
+            while (h.next != null) {
+                if (i == position - 1) {
+                    prev = h;
+                } else if (i == position) {
+                    prev.next = h.next;
+                    return;
+                }
+                i++;
+                h = h.next;
+            }
+            System.out.println("Position out of range!");
+
+        }
+    }
+
+    void deleteLast() {
+        Node h = head;
+        Node prev = null;
+        if (h != null) {
+            while (h.next != null) {
+                prev = h;
+                h = h.next;
+            }
+            prev.next = null;
+            tail = prev;
+        } else {
+            System.out.println("List empty!");
+        }
+    }
+
+    void deleteFirst() {
+        Node h = head;
+        if (h != null) {
+            head = h.next;
+        } else {
+            System.out.println("List empty!");
+        }
+    }
+
+    int exist(int val) {
+        Node h = head;
+        if (head == null) {
+            System.out.println("MyList is empty");
+        } else {
+            while (h.next != null) {
+                if (h.val == val) {
+                    return h.val;
+                }
+                h = h.next;
+            }
+        }
+        return -1;
+    }
+
+    void printList() {
+        Node h = head;
+        if (h == null) {
+            System.out.println("MyList is empty!");
+        } else {
+            while (h.next != null) {
+                System.out.println(h.val);
+                h = h.next;
+            }
+            System.out.println(h.val);
+        }
+    }
+
+}
